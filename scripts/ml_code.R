2069ee5ff15a30264cfdac20519aa7124be587f9
MattBixley <mattbixley72@gmail.com>
2019-11-06 08:41:09 +1300

    setting up examples

---
+++ scripts/ml_code.R
@@ -0,0 +1,10 @@
+## keras/tensor flow
+
+## inception v3
+# https://keras.rstudio.com/reference/application_inception_v3.html
+
+application_inception_v3(include_top = TRUE, weights = "imagenet",
+                         input_tensor = NULL, input_shape = NULL, pooling = NULL,
+                         classes = 1000)
+
+inception_v3_preprocess_input(x)
