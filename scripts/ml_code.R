f54be7b07c25a4a7c6d7255a81d65cf1d538dda7
MattBixley <mattbixley72@gmail.com>
2019-12-20 11:52:24 +1300

    inception not running

--- scripts/ml_code.R
+++ scripts/ml_code.R
@@ -3,7 +3,7 @@
 ## inception v3
 # https://keras.rstudio.com/reference/application_inception_v3.html
 
-application_inception_v3(include_top = TRUE, weights = "imagenet",
+conv_base <- application_inception_v3(include_top = TRUE, weights = "imagenet",
                          input_tensor = NULL, input_shape = NULL, pooling = NULL,
                          classes = 1000)
 
