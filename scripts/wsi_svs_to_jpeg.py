d5b54850a5cdf9c3480599dd77e92141e98e4d15
Matt Bixley <mattbixley72@gmail.com>
2020-04-28 18:21:56 +1200

    mod py

--- scripts/wsi_svs_to_jpeg.py
+++ scripts/wsi_svs_to_jpeg.py
@@ -37,13 +37,13 @@ class ResolutionLevel(IntEnum):
 
 def get_SVS_level_ratio(resolution_level):
   if resolution_level == ResolutionLevel.LEVEL_0_BASE:
-  return SVSLevelRatio.LEVEL_0_BASE
+    return SVSLevelRatio.LEVEL_0_BASE
 elif resolution_level == ResolutionLevel.LEVEL_1:
-  return SVSLevelRatio.LEVEL_1
+    return SVSLevelRatio.LEVEL_1
 elif resolution_level == ResolutionLevel.LEVEL_2:
-  return SVSLevelRatio.LEVEL_2
+    return SVSLevelRatio.LEVEL_2
 elif resolution_level == ResolutionLevel.LEVEL_3:
-  return SVSLevelRatio.LEVEL_3
+    return SVSLevelRatio.LEVEL_3
 
 
 def get_start_positions(width, height, window_size, axis, overlapping_percentage):
