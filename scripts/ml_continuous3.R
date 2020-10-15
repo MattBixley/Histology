8a4f1ff38b035c63a9601688c7ee4ea6e9c9984e
MattBixley <mattbixley72@gmail.com>
2020-10-15 13:19:43 +1300

    continuous

--- scripts/ml_continuous3.R
+++ scripts/ml_continuous3.R
@@ -1,19 +1,162 @@
-# https://navoshta.com/end-to-end-deep-learning/
+# https://www.pyimagesearch.com/2019/01/28/keras-regression-and-cnns/
 
-#https://github.com/navoshta/behavioral-cloning
+# As you’ll find out in the rest of today’s tutorial, performing regression with CNNs and Keras is as simple as:
+  
+# Removing the fully-connected softmax classifier layer typically used for classification
+# Replacing it with a fully-connected layer with a single node along with a linear activation function.
+# Training the model with a continuous value prediction loss function such as mean squared error, mean absolute error, mean absolute percentage error, etc.
 
-from keras import models
-from keras.layers import core, convolutional, pooling
+cd ~
+git clone https://github.com/emanhamed/Houses-dataset
+tree --dirsfirst
 
-model = models.Sequential()
-model.add(convolutional.Convolution2D(16, 3, 3, input_shape=(32, 128, 3), activation='relu'))
-model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
-model.add(convolutional.Convolution2D(32, 3, 3, activation='relu'))
-model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
-model.add(convolutional.Convolution2D(64, 3, 3, activation='relu'))
-model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
-model.add(core.Flatten())
-model.add(core.Dense(500, activation='relu'))
-model.add(core.Dense(100, activation='relu'))
-model.add(core.Dense(20, activation='relu'))
-model.add(core.Dense(1))
+## merge images back to one, don't need to do
+
+def load_house_images(df, inputPath):
+  # initialize our images array (i.e., the house images themselves)
+  images = []
+# loop over the indexes of the houses
+for i in df.index.values:
+  # find the four images for the house and sort the file paths,
+  # ensuring the four are always in the *same order*
+  basePath = os.path.sep.join([inputPath, "{}_*".format(i + 1)])
+housePaths = sorted(list(glob.glob(basePath)))
+
+# initialize our list of input images along with the output image
+# after *combining* the four input images
+inputImages = []
+outputImage = np.zeros((64, 64, 3), dtype="uint8")
+# loop over the input house paths
+for housePath in housePaths:
+  # load the input image, resize it to be 32 32, and then
+  # update the list of input images
+  image = cv2.imread(housePath)
+image = cv2.resize(image, (32, 32))
+inputImages.append(image)
+
+# tile the four input images in the output image such the first
+# image goes in the top-right corner, the second image in the
+# top-left corner, the third image in the bottom-right corner,
+# and the final image in the bottom-left corner
+outputImage[0:32, 0:32] = inputImages[0]
+outputImage[0:32, 32:64] = inputImages[1]
+outputImage[32:64, 32:64] = inputImages[2]
+outputImage[32:64, 0:32] = inputImages[3]
+# add the tiled image to our set of images the network will be
+# trained on
+images.append(outputImage)
+# return our set of images
+return np.array(images)
+
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
+  # initialize the input shape and channel dimension, assuming
+  # TensorFlow/channels-last ordering
+  inputShape = (height, width, depth)
+chanDim = -1
+
+# define the model input
+inputs = Input(shape=inputShape)
+# loop over the number of filters
+for (i, f) in enumerate(filters):
+  # if this is the first CONV layer then set the input
+  # appropriately
+  if i == 0:
+  x = inputs
+# CONV => RELU => BN => POOL
+x = Conv2D(f, (3, 3), padding="same")(x)
+x = Activation("relu")(x)
+x = BatchNormalization(axis=chanDim)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# flatten the volume, then FC => RELU => BN => DROPOUT
+x = Flatten()(x)
+x = Dense(16)(x)
+x = Activation("relu")(x)
+x = BatchNormalization(axis=chanDim)(x)
+x = Dropout(0.5)(x)
+# apply another FC layer, this one to match the number of nodes
+# coming out of the MLP
+x = Dense(4)(x)
+x = Activation("relu")(x)
+# check to see if the regression node should be added
+if regress:
+  x = Dense(1, activation="linear")(x)
+# construct the CNN
+model = Model(inputs, x)
+# return the CNN
+return model
+
+
+# import the necessary packages
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from pyimagesearch import datasets
+from pyimagesearch import models
+import numpy as np
+import argparse
+import locale
+import os
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", type=str, required=True,
+                help="path to input dataset of house images")
+args = vars(ap.parse_args())
+
+# construct the path to the input .txt file that contains information
+# on each house in the dataset and then load the dataset
+print("[INFO] loading house attributes...")
+inputPath = os.path.sep.join([args["dataset"], "HousesInfo.txt"])
+df = datasets.load_house_attributes(inputPath)
+# load the house images and then scale the pixel intensities to the
+# range [0, 1]
+print("[INFO] loading house images...")
+images = datasets.load_house_images(df, args["dataset"])
+images = images / 255.0
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+split = train_test_split(df, images, test_size=0.25, random_state=42)
+(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
+
+# find the largest house price in the training set and use it to
+# scale our house prices to the range [0, 1] (will lead to better
+# training and convergence)
+maxPrice = trainAttrX["price"].max()
+trainY = trainAttrX["price"] / maxPrice
+testY = testAttrX["price"] / maxPrice
+# create our Convolutional Neural Network and then compile the model
+# using mean absolute percentage error as our loss, implying that we
+# seek to minimize the absolute percentage difference between our
+# price *predictions* and the *actual prices*
+model = models.create_cnn(64, 64, 3, regress=True)
+opt = Adam(lr=1e-3, decay=1e-3 / 200)
+model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
+# train the model
+print("[INFO] training model...")
+model.fit(x=trainImagesX, y=trainY, 
+          validation_data=(testImagesX, testY),
+          epochs=200, batch_size=8)
+
+# make predictions on the testing data
+print("[INFO] predicting house prices...")
+preds = model.predict(testImagesX)
+# compute the difference between the *predicted* house prices and the
+# *actual* house prices, then compute the percentage difference and
+# the absolute percentage difference
+diff = preds.flatten() - testY
+percentDiff = (diff / testY) * 100
+absPercentDiff = np.abs(percentDiff)
+# compute the mean and standard deviation of the absolute percentage
+# difference
+mean = np.mean(absPercentDiff)
+std = np.std(absPercentDiff)
+# finally, show some statistics on our model
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+print("[INFO] avg. house price: {}, std house price: {}".format(
+  locale.currency(df["price"].mean(), grouping=True),
+  locale.currency(df["price"].std(), grouping=True)))
+print("[INFO] mean: {:.2f}%, std: {:.2f}%".format(mean, std))
+
+
+python cnn_regression.py --dataset ~/Houses-dataset/Houses\ Dataset/
+  
+  
